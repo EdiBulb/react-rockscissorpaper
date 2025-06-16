327b5694356ca048ff04058098945b62db08d339
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-06-16 22:41:22 +1200

    Create useState to change the user's choice and Randomize the choice of computer.

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Box = (props) => {
-    console.log("title: ",props)
+    console.log("props: ",props)
   return (
     <div className='box'>
+      {/* title은 가드값이 필요없음, 직접적으로 You 값을 줘서, but, item은 useSelect로 가져오기 때문에 초기값이 null로 설정되어있어서 가드값이 필요한 것 */}
       <h1>{props.title}</h1>
-      <img className='item-img' src='https://m.media-amazon.com/images/I/51QtmdQD-sL.jpg'/>
-      <p>result</p>
+      {/* 에러 방지로 가드값을 쓴다: props.item이다. props.item 이 없으면 false니까 저절로 안되고, 있으면 img 가 있으니까,  */}
+      <img className='item-img' src={props.item && props.item.img}/> 
+      <p>WIN</p>
     </div>
   )
 }
